bf78d54dd42f7779d571182acfb7e58fcacfc44b
RUzai <RUzairov@gmail.com>
2023-03-04 18:36:04 +0300

    build

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
+import 'package:nfc_manager/nfc_manager.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
@@ -22,8 +24,71 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
+  void readKit() {
+    FlutterNfcKit.poll().then(Logs.logsStream);
+  }
+
+  void readManager() {
+    NfcManager.instance.startSession(
+      onDiscovered: (tag) async => Logs.logsStream(tag),
+    );
+  }
+
+  void show(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) => const Logs(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center());
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: readKit,
+              child: const Text('Flutter NFC Kit'),
+            ),
+            ElevatedButton(
+              onPressed: readManager,
+              child: const Text('NFC Manager'),
+            ),
+          ],
+        ),
+      ),
+      bottomSheet: InkWell(
+        onTap: () => show(context),
+        child: Ink(
+          color: Colors.blue[200],
+          height: 60,
+          child: const Center(
+            child: Text('Logs'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Logs extends StatelessWidget {
+  const Logs({super.key});
+
+  static Stream<String> logsStream(value) async* {
+    yield value.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      heightFactor: 0.7,
+      child: StreamBuilder<String>(
+        builder: (_, snap) {
+          return Text(snap.data ?? '');
+        },
+      ),
+    );
   }
 }
